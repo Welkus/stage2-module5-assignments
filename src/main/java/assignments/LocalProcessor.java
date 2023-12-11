d1389418445d733e1d6f4d9f0768d7625cbb3cc7
Welkus <vdelkus@gmail.com>
2023-12-11 13:58:07 +0200

    Test 2

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -15,75 +15,29 @@ import lombok.Setter;
 @Setter
 public class LocalProcessor {
     private String processorName;
-    private Long period = 10000000000000L;
-    protected String ProcessorVersion;
+    private Long period = 10_000_000_000_000L;
+    protected String processorVersion;
     private Integer valueofCheap;
     private Scanner informationscanner;
-   private static LinkedList<String> stringArrayList = new LinkedList<>();
+   private static LinkedList<String> stringArrayList;
 
     public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
                           Scanner informationscanner, LinkedList<String> stringArrayList) {
         this.processorName = processorName;
         this.period = period;
-        ProcessorVersion = processorVersion;
+        this.processorVersion = processorVersion;
         this.valueofCheap = valueOfCheap;
         this.informationscanner = informationscanner;
         this.stringArrayList = stringArrayList;
     }
 
     public LocalProcessor() {
+        this.stringArrayList = new LinkedList<>();
     }
 
-    public String getProcessorName() {
-        return processorName;
-    }
-
-    public void setProcessorName(String processorName) {
-        this.processorName = processorName;
-    }
-
-    public Long getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Long period) {
-        this.period = period;
-    }
-
-    public String getProcessorVersion() {
-        return ProcessorVersion;
-    }
-
-    public void setProcessorVersion(String processorVersion) {
-        ProcessorVersion = processorVersion;
-    }
-
-    public Integer getValueofCheap() {
-        return valueofCheap;
-    }
-
-    public void setValueofCheap(Integer valueofCheap) {
-        this.valueofCheap = valueofCheap;
-    }
-
-    public Scanner getInformationscanner() {
-        return informationscanner;
-    }
-
-    public void setInformationscanner(Scanner informationscanner) {
-        this.informationscanner = informationscanner;
-    }
-
-    public static LinkedList<String> getStringArrayList() {
-        return stringArrayList;
-    }
-
-    public static void setStringArrayList(LinkedList<String> stringArrayList) {
-        LocalProcessor.stringArrayList = stringArrayList;
-    }
 
     @ListIteratorAnnotation
-    public void listiterator(LinkedList<String> stringList) {
+    public void listIterator(LinkedList<String> stringList) {
         stringArrayList = new LinkedList<>(stringList);
         for (int i = 0; i < period; i++) {
             System.out.println(stringArrayList.get(i).hashCode());
@@ -102,7 +56,7 @@ public class LocalProcessor {
     public void readFullProcessorName(File file) throws FileNotFoundException {
             informationscanner = new Scanner(file);
             while (informationscanner.hasNext()) {
-                ProcessorVersion+= informationscanner.nextLine();
+                processorVersion += informationscanner.nextLine();
             }
 
     }
